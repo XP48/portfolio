4119fd276bf9e74df4cc8d81442d779b9adc8969
Antonin Lancesseur <antoninlancesseur@gmail.com>
2024-04-11 11:46:33 +0200

    ajout des animations au chargement

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// Animations au scroll
+
+const ratio = .1
+
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: ratio
+}
+
+const handleIntersect = function (entries, observer) {
+    entries.forEach(function (entry) {
+        if(entry.intersectionRatio > ratio) {
+            entry.target.classList.remove('reveal')
+            observer.unobserve(entry.target)
+        } else {
+
+        }
+    })
+}
+
+const observer = new IntersectionObserver(handleIntersect, options)
+document.querySelectorAll('.reveal').forEach(function (r) {
+    observer.observe(r)
+})
