c0cf22044c1b488f5480541e61fc3681c42ceac3
antoninlancesseur@gmail.com <antoninlancesseur@gmail.com>
2024-06-26 22:39:11 +0200

    systeme de tri avec les boutons

--- js/main.js
+++ js/main.js
@@ -21,3 +21,15 @@ const observer = new IntersectionObserver(handleIntersect, options)
 document.querySelectorAll('[class*="reveal-"]').forEach(function (r) {
     observer.observe(r)
 })
+
+
+// fonctions des boutons
+const caseInGrid = document.querySelectorAll('.case')
+
+function highlight(classe) {
+    caseInGrid.forEach(element => {
+        element.classList.remove('masked')
+        if(!element.matches(classe))
+            element.classList.add('masked')
+    });
+}
